f85f04d171cf67f5c3db39729020aa1e62581901
Johan Legreid <johanleg@stud.ntnu.no>
2024-03-11 15:41:31 +0100

    Resolve "Bug: marking last card as hard"

--- client/components/flashcard.tsx
+++ client/components/flashcard.tsx
@@ -17,6 +17,9 @@ import {
   JSONToFlashcardSet,
 } from "@/state/zustand";
 import "../styles/flashcard.css";
+import ReactCardFlip from "react-card-flip";
+import { useRouter } from "next/navigation";
+import ConfettiExplosion from "react-confetti-explosion";
 
 interface FlashcardRef {
   nextCard: () => void;
@@ -25,7 +28,6 @@ interface FlashcardRef {
 }
 
 export const FlashCard = forwardRef<FlashcardRef, {}>(() => {
-  const [isFlipped, setIsFlipped] = useState(false);
   const [flashcardSet, setFlashcardSet] = useState<flashcardSet | null>(null);
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const { setname, setSet } = toggleSet();
@@ -34,7 +36,15 @@ export const FlashCard = forwardRef<FlashcardRef, {}>(() => {
     prevCard: () => {},
     resetArray: () => {},
   })!;
+
   const [cards, setCards] = useState<Flashcard[]>([]); // Use state to trigger re-render when cards change
+  const [isFlipped, setIsFlipped] = useState(false);
+  const router = useRouter();
+  const [isExploding, setIsExploding] = React.useState(false);
+  const [isFinished, setIsFinished] = useState(false);
+  const [displayedCardIndex, setDisplayedCardIndex] = useState<number | null>(
+    null
+  );
 
   const defaultFrontContentStyle: CSSProperties = {
     display: "flex",
@@ -51,38 +61,6 @@ export const FlashCard = forwardRef<FlashcardRef, {}>(() => {
   }
 
   let num = 1;
-
-  const markAsHard = () => {
-    if (!flashcardSet) {
-      return;
-    }
-    console.log(flashcardSet.flashcards);
-    const newFlashcardSet = [...flashcardSet.flashcards];
-    const currentCard = newFlashcardSet[currentCardIndex];
-
-    //Removes(splice) the card from its current position and pushes it to the end
-    newFlashcardSet.splice(currentCardIndex, 1);
-    newFlashcardSet.push(currentCard);
-    setFlashcardSet({ ...flashcardSet, flashcards: newFlashcardSet });
-    const newCardss: Flashcard[] = newFlashcardSet.map((flashcard) => ({
-      id: num++,
-      frontHTML: flashcard.question,
-      backHTML: flashcard.answer,
-      frontContentStyle: defaultFrontContentStyle,
-      backContentStyle: defaultFrontContentStyle,
-    }));
-    setCards(newCardss);
-    setIsFlipped(false);
-  };
-
-  const randomizeFlashcards = (flashcardlist: Flashcard[]) => {
-    const copyList = [...flashcardlist];
-    for (let i = copyList.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [copyList[i], copyList[j]] = [copyList[j], copyList[i]];
-    }
-    return copyList;
-  };
 
   useEffect(() => {
     const fetchFlashcards = async () => {
@@ -114,7 +92,7 @@ export const FlashCard = forwardRef<FlashcardRef, {}>(() => {
               backContentStyle: defaultFrontContentStyle,
             })
           );
-          setCards(randomizeFlashcards(newCards));
+          setCards(newCards);
         } else {
           const flashcardSetObject =
             flashcardSets[
@@ -128,9 +106,10 @@ export const FlashCard = forwardRef<FlashcardRef, {}>(() => {
               backHTML: flashcard.answer,
               frontContentStyle: defaultFrontContentStyle,
               backContentStyle: defaultFrontContentStyle,
+              isFlipped: false,
             })
           );
-          setCards(randomizeFlashcards(newCards));
+          setCards(newCards);
         }
         console.log(setname);
       } catch (error) {
@@ -147,22 +126,74 @@ export const FlashCard = forwardRef<FlashcardRef, {}>(() => {
   //     frontHTML: flashcard.question,
   //     backHTML: flashcard.answer,
   //     frontContentStyle: defaultFrontContentStyle,
-  //     backContentStyle: defaultFrontContentStyle
+  //     backContentStyle: defaultFrontContentStyle,
   //   }));
-  //   console.log(newCards)
-  //   setCards(newCards);
+  //   console.log(newCards);
+  //   setCards(randomizeFlashcards(newCards));
   // };
 
   const goToNextCard = () => {
-    flashcardArrayRef.current?.nextCard();
-    setCurrentCardIndex((prevIndex) =>
-      Math.min(prevIndex + 1, flashcardSet?.flashcards.length ?? 0 - 1)
+    setTimeout(() => {
+      if (currentCardIndex < flashcardSet.flashcards.length - 1) {
+        flashcardArrayRef.current?.nextCard();
+        setIsFlipped(false);
+        setCurrentCardIndex((prevIndex) =>
+          Math.min(prevIndex + 1, flashcardSet.flashcards.length - 1)
+        );
+        setDisplayedCardIndex(currentCardIndex);
+      } else {
+        setIsFinished(true);
+        setIsExploding(true);
+      }
+    }, 130);
+  };
+
+  const markAsHard = () => {
+    setIsFlipped(false);
+
+    if (currentCardIndex !== undefined) {
+      const currentCard = cards[currentCardIndex];
+      const newCards = [...cards];
+
+      newCards.splice(currentCardIndex, 1);
+      newCards.push(currentCard);
+      setCards(newCards);
+    }
+  };
+
+  const randomizeFlashcards = () => {
+    const remainingCards = cards.filter(
+      (_, index) => index !== displayedCardIndex
     );
+    const copyList = [...remainingCards];
+
+    for (let i = copyList.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copyList[i], copyList[j]] = [copyList[j], copyList[i]];
+    }
+
+    if (displayedCardIndex !== null) {
+      copyList.splice(displayedCardIndex, 0, cards[displayedCardIndex]);
+    }
+    return copyList;
   };
 
   const goToPrevCard = () => {
+    setIsFlipped(false);
     flashcardArrayRef.current?.prevCard();
     setCurrentCardIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+  };
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  const exitSession = () => {
+    router.push("/allSets");
+  };
+
+  const shuffleCards = () => {
+    setCards(randomizeFlashcards());
   };
 
   useEffect(() => {
@@ -170,6 +201,14 @@ export const FlashCard = forwardRef<FlashcardRef, {}>(() => {
       flashcardArrayRef.current.resetArray();
     }
   }, [flashcardSet]);
+
+  useEffect(() => {
+    if (isExploding) {
+      setTimeout(() => {
+        setIsExploding(false);
+      }, 2000);
+    }
+  }, [isExploding]);
 
   if (!flashcardSet) {
     return <Spinner></Spinner>;
@@ -195,6 +234,86 @@ export const FlashCard = forwardRef<FlashcardRef, {}>(() => {
   const progressValue =
     ((currentCardIndex + 1) / flashcardSet.flashcards.length) * 100;
 
+  if (isFinished) {
+    return (
+      <div>
+        {isExploding && <ConfettiExplosion />}
+        <div className="text-center text-4xl font-bold mt-5">
+          Congratulations! You have finished the set!
+        </div>
+        <Button
+          name="exitButton"
+          color="danger"
+          className="mt-5"
+          onClick={exitSession}
+        >
+          Go Back To Sets
+        </Button>
+      </div>
+    );
+  }
+
+  if (currentCardIndex === flashcardSet.flashcards.length - 1) {
+    return (
+      <div>
+        <Button
+          name="hardButton"
+          color="primary"
+          className="mb-5"
+          onClick={markAsHard}
+        >
+          Mark as difficult
+        </Button>
+
+        <div className=" flex flex-row items-center justify-center">
+          <Button
+            variant="ghost"
+            className="border border-none"
+            onClick={goToPrevCard}
+          >
+            <span className="text-2xl">&larr;</span>
+          </Button>
+
+          <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+            <div className="card" onClick={handleClick}>
+              {cards[currentCardIndex]?.frontHTML}
+            </div>
+
+            <div className="card" onClick={handleClick}>
+              {cards[currentCardIndex]?.backHTML}
+            </div>
+          </ReactCardFlip>
+
+          <Button
+            onClick={goToNextCard}
+            variant="ghost"
+            className="border border-none"
+            // disabled={currentCardIndex >= flashcardSet.flashcards.length - 1}
+          >
+            Finish set
+          </Button>
+        </div>
+        <div className="flex items-center justify-center content-center mt-3">
+          <Progress
+            isStriped
+            aria-label="Progress"
+            color="warning"
+            value={progressValue}
+            className="max-w-md"
+          />
+        </div>
+
+        <Button
+          name="exitButton"
+          color="danger"
+          className="mt-5"
+          onClick={exitSession}
+        >
+          Exit session
+        </Button>
+      </div>
+    );
+  }
   return (
     <div>
       <Button
@@ -205,20 +324,26 @@ export const FlashCard = forwardRef<FlashcardRef, {}>(() => {
       >
         Mark as difficult
       </Button>
+
       <div className=" flex flex-row items-center justify-center">
         <Button
           variant="ghost"
           className="border border-none"
           onClick={goToPrevCard}
-          // disabled={currentCardIndex <= 0}
         >
           <span className="text-2xl">&larr;</span>
         </Button>
-        <FlashcardArray
-          controls={false}
-          forwardRef={flashcardArrayRef}
-          cards={cards}
-        />
+
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+          <div className="card" onClick={handleClick}>
+            {cards[currentCardIndex]?.frontHTML}
+          </div>
+
+          <div className="card" onClick={handleClick}>
+            {cards[currentCardIndex]?.backHTML}
+          </div>
+        </ReactCardFlip>
+
         <Button
           onClick={goToNextCard}
           variant="ghost"
@@ -237,6 +362,24 @@ export const FlashCard = forwardRef<FlashcardRef, {}>(() => {
           className="max-w-md"
         />
       </div>
+
+      <Button
+        name="exitButton"
+        color="danger"
+        className="mt-5 mr-10"
+        onClick={exitSession}
+      >
+        Exit session
+      </Button>
+
+      <Button
+        name="shuffleButton"
+        color="secondary"
+        className="shuffle-button"
+        onClick={shuffleCards}
+      >
+        Shuffle cards
+      </Button>
     </div>
   );
 });
